c8942523a4551013866aade180f73b639679402f
Sheal354 <Sheal2004@gmail.com>
2024-06-04 15:49:41 +0400

    фФФФФФФФФ

--- app/src/main/java/com/example/dawnofdesolation/MainActivity.java
+++ app/src/main/java/com/example/dawnofdesolation/MainActivity.java
@@ -23,12 +23,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
         prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
         editTextNickname = findViewById(R.id.edittext_nickname);
 
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
