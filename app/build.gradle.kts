61e6e771960c3a356161c6d1a6aa995735a6fbaf
Dawn_Des <polina121212@bk.ru>
2024-06-06 14:07:46 +0400

    stage something (day 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    alias(libs.plugins.androidApplication)
     id("com.android.application")
     id("com.google.gms.google-services")
 }
--- app/src/main/java/com/example/dawnofdesolation/LeaderboardActivity.java
+++ app/src/main/java/com/example/dawnofdesolation/LeaderboardActivity.java
@@ -1,6 +1,14 @@
 package com.example.dawnofdesolation;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class LeaderboardActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+    }
+
 }
